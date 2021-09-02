f59bd2a519222d59579eea83d0e5f0f70dc2085a
Jose Claudio Tomas Jr <jclaudiotomas@gmail.com>
2021-09-02 01:51:57 -0300

    Bateria e Clima JS

---
+++ Bateria/script.js
@@ -0,0 +1,41 @@
+document.body.addEventListener("keyup", (Event) =>{
+    playSound(Event.code.toLocaleLowerCase())
+    console.log(Event.code.toLocaleLowerCase())
+})
+document.querySelector(".composer button").addEventListener("click", () => {
+    let song = document.querySelector("#input").value
+    if(song !== "") {
+        let songArray = song.split("")
+        playComposition(songArray)
+    }
+})
+document.querySelectorAll('.key').forEach(element => {
+    element.addEventListener('click', () => {
+        const clickedKey = element.getAttribute('data-key')
+        playSound(clickedKey)
+    })
+})
+function playSound(sound) { 
+    let audioElement = document.querySelector(`#s_${sound}`)
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+    if(audioElement) {
+        audioElement.currentTime = 0
+        audioElement.play()
+    }
+    if(keyElement) {
+        keyElement.classList.add("active")
+        setTimeout(() => {
+            keyElement.classList.remove("active")
+        }, 300)
+    }
+}
+
+function playComposition(songArray) {
+    let wait = 0
+    for(let songItem of songArray) {
+        setTimeout(() => {
+            playSound(`key${songItem}`)
+        }, wait)
+        wait += 125
+    }
+}
